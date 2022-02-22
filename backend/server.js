12b07c8f2aa9433dbddc6afb7a8ece6952ab3c44
JanGolabek <janekgolabekk@hotmail.com>
2022-02-22 18:36:19 +0100

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const colors = require('colors')
+const connectDB = require('./config/db')
 const req = require('express/lib/request')
 const { get } = require('express/lib/response')
 const res = require('express/lib/response')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+
+connectDB()
 
 const app = express()
 
